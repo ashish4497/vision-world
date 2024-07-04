0ad9eb806869d552ab57bee8aa31a9c86f855ef7
ashish4497 <ak8393267@gmail.com>
2024-07-04 10:43:33 +0530

    react-redux-basic-setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {  increment, decrement, incrementByAmount, selectCount } from './slice/counterSlice';
+import HeroPage from './screens/heroPage';
+
+
+
 function App() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="counter">
+        <p>Count: {count}</p>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>Increment by 2</button>
       </header>
+      <HeroPage/>
     </div>
   );
 }
 
 export default App;
+
